62feaf80be8bea11feccec0fe854c2a35d961568
jessikea <jessikea@jtran.git>
2022-10-30 22:26:28 -0400

    randomizer for numbers added

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
 
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
 var numbers= "1234567890";
 var upperCaseChars= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCaseChars= "abcdefghijklmopqrstuvwxyz";
 var specialChars= "!@#$%^&*()_+-=[];',./?><|:`~}{"
 
 
-function randomizer (text, numbers = 1){
+function randomizer(text, numbers = 1) {
   let randomString = "";
 
-  for (i = 0; i> numbers; i++){
-    randomString += text.charAt(Math.floor(Math.random()*text.length));
+  for (let i = 0; i < numbers; i++) {
+    randomString += text.charAt(Math.floor(Math.random() * text.length));
   }
-return randomString
+
+  return randomString;
 }
 
 function generatePassword() { 
-  var passwordLength = parseInt(
-    prompt("Indicate length of password. Must be a minimum of 8 characters and a maximum of 128 characters")
+  let IncludeChars = "";
+  var passwordGenerated= "";  
 
+  let passwordLength = parseInt(
+    prompt("Indicate length of password. Must be a minimum of 8 characters and a maximum of 128 characters")
   );
-  if (isNaN(passwordLength)|| passwordLength<8 || passwordLength>128){
+
+  if (isNaN(passwordLength)|| passwordLength <8 || passwordLength>128){
     alert ("Number entered is not within required range for password length");
     return "";
   }
 
-  includeNumberChars= confirm ("confirm to include numbers in generated password"
+  includeNumberChars= confirm ("Confirm to include numbers in generated password"
   );
-  includeUpperCaseChars= confirm ("confirm to include upper case characters in generated password"
+  includeUpperCaseChars= confirm ("Confirm to include upper case characters in generated password"
   ); 
-  includeLowerCaseChars= confirm ("confirm to include lower case characters in generated password"
+  includeLowerCaseChars= confirm ("Confirm to include lower case characters in generated password"
   ); 
-  includeSpecialChars= confirm ("confirm to include special characters in generated password"
+  includeSpecialChars= confirm ("Confirm to include special characters in generated password"
   );
 
+  if (includeNumberChars ) {
+    IncludeChars += numbers;
+    passwordGenerated += randomizer(numbers);
   }
-}
 
+  passwordGenerated += randomizer(
+    IncludeChars, passwordLength - passwordGenerated.length
+  );
+  passwordGenerated = randomizer(
+    passwordGenerated, passwordGenerated.length);
 
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  return passwordGenerated;
+  }
 
 // Write password to the #password input
 function writePassword() {
