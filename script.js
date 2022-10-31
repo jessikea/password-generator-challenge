8fd5bf4c0cc39bc52012fc61b423493fa3194f8d
jessikea <jessikea@jtran.git>
2022-10-30 20:58:18 -0400

    password length prompt added

---
+++ script.js
@@ -0,0 +1,45 @@
+
+// Assignment code here
+var numbers= "1234567890";
+var upperCaseChars= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseChars= "abcdefghijklmopqrstuvwxyz";
+var specialChars= "!@#$%^&*()_+-=[];',./?><|:`~}{"
+
+
+function randomizer (text, numbers = 1){
+  let randomString = "";
+
+  for (i = 0; i> numbers; i++){
+    randomString += text.charAt(Math.floor(Math.random()*text.length));
+  }
+return randomString
+}
+
+function generatePassword() { 
+  var passwordLength = parseInt(
+    prompt("Indicate length of password. Must be a minimum of 8 characters and a maximum of 128 characters")
+
+  );
+  if (isNaN(passwordLength)|| passwordLength<8 || passwordLength>128){
+    alert ("Number entered is not within required range for password length");
+    return "";
+  }
+}
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
